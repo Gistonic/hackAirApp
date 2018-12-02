6291fe12170e50fad7847de3d0b4788524d640b4
Sergio <sergio.paredes@fib.upc.edu>
2018-12-02 11:25:47 +0200

    Update envelopes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import {
   Image,
-  Platform,
-  ScrollView,
   StyleSheet,
-  Text,
-    ImageBackground,
   TouchableOpacity,
   View,
 } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
-import { WebBrowser } from 'expo';
+import { createAppContainer } from 'react-navigation';
 
-import { MonoText } from '../components/StyledText';
-import MenuButton from "../components/MenuButton";
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -46,14 +39,12 @@ export default class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
     questsContainer: {
         width: '100%',
-        paddingLeft: 10,
-        paddingTop: 300,
         flexDirection: 'row',
         flexWrap: 'wrap',
         justifyContent: 'center'
     },
     img:{
-
+        marginTop: 50,
     }
 });
 
